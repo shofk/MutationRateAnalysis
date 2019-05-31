2df808eb985bd10c1387b1e1096a5a505d33e035
Shohei <shofk26@gmail.com>
2019-05-31 12:01:23 -0700

    Documented

--- get_mutation_rate_updated.m
+++ get_mutation_rate_updated.m
@@ -1,19 +1,33 @@
+% Mutation Rate Data Processing & Mutation Rate Calculating
+% @Usage: Run, then the program will ask if the user wants to run the
+% processing for BST only data (0) or Taq+BST data (1). Type in the choice
+% and press "Enter".
+% @parameter that can be changed:
+%   directories of POI tables should be correctly given
+%   out_dir
+%   graph graphs if True
+%   save_image Saves called mutation map
+
+% Last Modified: 5/31/2019
+
+
+%%  Setups
 out_dir = 'D:\\MutationRateProject\\_20190529_%s_analysis';
 isTaq = input('Taq:1 or BST:0?');
 
 
 max_fov = 120;
 graph_ = false;
-save_image = true;
+save_image = false;
 
 
 if isTaq
     directory = 'D:\MutationRateProject\Taq_local_max\Point_of_Interest_table';
     out_dir = sprintf(out_dir, 'Taq');
-    ratios_low = ones(3, 12) * 0.26;
-    ratios_high = ones(3, 12) * 1.50;
-    to_gta_cutoff = 0.30;
-    to_c_cutoff = 0.4;
+    ratios_low = ones(3, 12) * 0.26;  % Lower threshold
+    ratios_high = ones(3, 12) * 1.50; % Upper threshold
+    to_gta_cutoff = 0.30;             % Mutation to GTA threshold
+    to_c_cutoff = 0.4;                % Mutation to C threshold
     c_low = 0.50;
 else % isBST
     directory = 'D:\MutationRateProject\BST1_max_itst\Point_of_Interest_table';
@@ -55,7 +69,7 @@ if ~exist(out_dir, 'dir') && save_image
     mkdir(out_dir);
 end
 
-
+% Cycle and base setting
 cycles = cell(4,1);
 cycles{1} = [7, 10];      % G
 cycles{2} = [1, 4, 8, 11];% T
@@ -73,24 +87,31 @@ cyc = 1:12;
 poi_c = poi_master;
 b = {'G', 'T', 'A'};
 
+% Go over FOV by FOV, process data and count mutations
 for ifov = FOVs
     if save_image
         mkdir(fullfile(out_dir, sprintf('FOV%d', ifov)));
     end
     out_directory = fullfile(out_dir, sprintf('FOV%d', ifov));
+    % Find POI's from the FOV processing in the iteration
     poiLocs = find(poi_indices(:,1) == ifov);
     offset = poiLocs(1)-1;
     itst = extract_and_correct_phase2(poi((poiLocs(1)-1)*3+1:(poiLocs(end)-1)*3+3, :), graph_);
     median_itst = get_median_intensities(itst);
     iMut = cell(4, 12);
+    % Go through the FOV cycle by cycle and count mutations
     for icyc = cyc
+        % Variable to keep track of the number of POI's not going to be
+        % included in the total POI counts
         premut = zeros(poi_counts(ifov, icyc), 1);
+        % Find which channel is the true channel intensity
         for ich = 1:4
             if sum(ismember(cycles{ich}, icyc))
                 chN = ich;
                 break;
             end
         end
+        % Get intensities
         citst = reshape(itst(:, icyc), 3, numel(poiLocs))';
         medians = median(citst);
         medians = medians .* (medians < 0.5);
@@ -105,9 +126,6 @@ for ifov = FOVs
         if chN == 4 % when C cycle
             for iich = channel
                 mut_itst = citst(:, iich);
-%                 idx = find(mut_itst > c_low + median(mut_itst) & sum(citst < to_c_cutoff * median_itst, 2) ~= 3 ...
-%                     & mut_itst < c_high + median(mut_itst) ...
-%                     & sum(citst > c_low + median(mut_itst) & citst < c_high + median(mut_itst), 2) == 1) - 1 + offset;
                 idx = find(mut_itst > std(mut_itst) * 1.5 & sum(citst < to_gta_cutoff * median_itst, 2) == 2) - 1 + offset;
                 % record the number of mutations
                 iMut{iich, icyc} = idx;
@@ -154,13 +172,16 @@ for ifov = FOVs
                     fullfile(out_directory, sprintf(image_name, ifov, icyc, bases(chN), 'C')));
             end
         end
+        % Subtract the number of POI's that are not going to be included in
+        % the total POI's
         poi_counts(ifov, icyc) = poi_counts(ifov, icyc) - sum(premut);
     end
+    % Stroe mutation counts
     nMutations{ifov} = iMut;
     poi_c((poiLocs(1)-1)*3+1:(poiLocs(end)-1)*3+3, 4:end) = itst;
     fprintf('FOV%d complete\n', ifov);
 end
-%%
+%% Mutation Rate Calculation
 
 if just_one
     final_rate = compile_mutation_rate(nMutations, FOVs, poi_counts);
@@ -171,45 +192,6 @@ if just_one
     xlswrite(fullfile(out_dir, 'mutation_final_rate.xlsx'), final_rate);
     xlswrite(fullfile(out_dir, 'mutation_cycle_rate.xlsx'), cycle_mutation_rate);
 end
-%%
-% for icyc = cyc
-%     chN = 4;
-%     for ich = channel
-%         if ~isempty(intersect(icyc, cycles{ich}))
-%             chN = ich;
-%         end
-%     end
-%     if chN ~= 4
-%         main_itst = poi(chN:3:end, icyc);
-%         for ich = channel
-%             if chN ~= ich
-%                 mut_itst = poi(ich:3:end, icyc);
-%                 idx = find(mut_itst > main_itst * (1/3));
-%                 map = zeros(1024);
-%                 idx = intersect(idx, find(poi_indices(:, 1)==fov));
-%                 map(poi_indices(idx, 2)) = 1;
-%                 imwrite(imdilate(map, strel('disk', 10)), fullfile(out_directory, sprintf(image_name, fov, icyc, bases(ich))));
-%             end
-%         end
-%     else
-%         for ich = channel
-%             mut_itst = poi(ich:3:end, icyc);
-%             idx = find(mut_itst > (1/3));
-%             map = zeros(1024);
-%             idx = intersect(idx, find(poi_indices(:,1) == fov));
-%             map(poi_indices(idx, 2)) = 1;
-%             imwrite(imdilate(map, strel('disk', 10)), fullfile(out_directory, sprintf(image_name, fov, icyc, 'C')));
-%         end
-%     end
-%     
-%     
-% end
-
-
-
-
-
-
 disp('Complete');
 
 %% Functions
@@ -350,15 +332,6 @@ function [poi_c, phaseRatios] = extract_and_correct_phase2(poi, graph_)
     collectingCycles{3,2} = [4, 7, 10];
 
     channelIndex = [1, 2, 3];
-
-    %% acquire phase ratios
-    % for i = 1:3
-    %     mainCycle = median(poi(channelIndex(i):3:end, collectingCycles(i)));
-    %     preCycle = median(poi(channelIndex(i):3:end, collectingCycles(i) - 1));
-    %     postCycle = median(poi(channelIndex(i):3:end, collectingCycles(i) + 1));
-    %     phaseRatios(i, 1) = preCycle / mainCycle;
-    %     phaseRatios(i, 2) = postCycle / mainCycle;
-    % end
 
     x = 1:12;
     for i = 1:3
@@ -531,120 +504,8 @@ function [poi_c, phaseRatios] = extract_and_correct_phase(poi, graph_)
     end
 end
 
-
-%%
-% % phaseRatios = zeros(3,2);
-% phaseRatios = zeros(6, 12);
-% 
-% cycles = cell(4,1);
-% cycles{1} = [7, 10];      % G
-% cycles{2} = [1, 4, 8, 11];% T
-% cycles{3} = [3, 5, 6, 9]; % A
-% cycles{4} = [2, 12];      % C
-% 
-% % collectingCycles = [7, 4, 3]; % G T A
-% collectingCycles = cell(3, 2);
-% collectingCycles{1,1} = [6, 9];   % G pre
-% collectingCycles{1,2} = [8, 11];  % G post
-% collectingCycles{2,1} = [3, 7, 10];
-% collectingCycles{2,2} = [5, 9, 12];
-% collectingCycles{3,1} = [2, 4, 8];
-% collectingCycles{3,2} = [4, 7, 10];
-% 
-% channelIndex = [1, 2, 3];
-% 
-% %% acquire phase ratios
-% % for i = 1:3
-% %     mainCycle = median(poi(channelIndex(i):3:end, collectingCycles(i)));
-% %     preCycle = median(poi(channelIndex(i):3:end, collectingCycles(i) - 1));
-% %     postCycle = median(poi(channelIndex(i):3:end, collectingCycles(i) + 1));
-% %     phaseRatios(i, 1) = preCycle / mainCycle;
-% %     phaseRatios(i, 2) = postCycle / mainCycle;
-% % end
-% 
-% x = 1:12;
-% for i = 1:3
-%     preCycle = median(poi(channelIndex(i):3:end, collectingCycles{i, 1}), 1);
-%     postCycle = median(poi(channelIndex(i):3:end, collectingCycles{i, 2}), 1);
-%     pPre = polyfit(collectingCycles{i, 1}, preCycle, 1);
-%     pPost = polyfit(collectingCycles{i, 2}, postCycle, 1);
-%     phaseRatios(i, :) = polyval(pPre, x);
-%     phaseRatios(i+3, :) = polyval(pPost, x);
-% end
-% phaseRatios(phaseRatios < 0) = 0;
-% 
-% %% create phase correction matrix
-% % phasing_matricies = cell(3,1);
-% % for i = 1:3
-% %     phasing_matricies{i} = create_phasing_matrix(phaseRatios(i, 1), phaseRatios(i, 2), 12);
-% % end
-% 
-% % % Correct phasing
-% % poi_c = poi';
-% % poi_c = reshape(poi_c, 12, 3, nPoi);
-% % poi_c = permute(poi_c, [1, 3, 2]);
-% % for i = 1:3
-% %     poi_c(:,:,i) = phasing_matricies{i} \ poi_c(:,:,i);
-% % end
-% % 
-% % poi_c = permute(poi_c, [1, 3, 2]);
-% % poi_c = reshape(poi_c, 12, 3 * nPoi);
-% % poi_c = poi_c';
-% 
-% % Correct phasing
-% poi_c = poi;
-% for ich = 1:3
-%     % get intensities (put the first cycle to the front)
-%     % (itst:intensities)
-%     itst = poi_c(ich:3:end, :);
-% 
-%     % correct pre phasing of the first cycle
-%     itst(:,1) = correct_phasing(itst(:,1), itst(:,2), phaseRatios(ich, 1));
-% 
-%     % correct pre and post phasing of all middles cycles
-%     for icyc = 2:11
-%         % correct postphasing
-%         itst(:,icyc) = correct_phasing(itst(:, icyc), itst(:, icyc-1), phaseRatios(ich+3, icyc-1));
-%         % correct prephasing
-%         itst(:,icyc) = correct_phasing(itst(:,icyc), itst(:,icyc+1), phaseRatios(ich, icyc+1));
-%     end
-% 
-%     % correct post phasing of the last cycle
-%     itst(:, end) = correct_phasing(itst(:,end), itst(:,end-1), phaseRatios(ich+3, 12));
-% 
-%     % put back the intensities to the poi table
-%     poi_c(ich:3:end, :) = itst;
-% end
-% %% Plot
-% plot_intensities(poi);
-% % plot_intensities(poi_c);
-% 
-% %% Collect medians of signal cycles and normalize
-% normalizing_itst = zeros(3, 12);
-% cyc = 1:12;
-% for ich = 1:3
-%     normalizing_itst(ich, cycles{ich}) = median(poi(channelIndex(ich):3:end, cycles{ich}), 1);
-%     average = mean(normalizing_itst(ich, cycles{ich}));
-%     normalizing_itst(ich, cyc(~ismember(cyc, cycles{ich}))) = average;
-% end
-% 
-% poi_c = poi_c';
-% poi_c = reshape(poi_c, 12, 3, nPoi);
-% for i = 1:nPoi
-%     poi_c(:,:,i) = poi_c(:,:,i) ./ normalizing_itst';
-% end
-% 
-% poi_c = reshape(poi_c, 12, 3 * nPoi);
-% poi_c = poi_c';
-% 
-% % plot
-% plot_intensities(poi_c);
-% 
-% %% Clean up
-% clearvars -except poi poi_c poi_master out_directory directory cyc cycles max_fov
-
-
 function final_rate = compile_mutation_rate(mutation_table, fov_list, poi_counts)
+    % Counts total mutation counts
     count = zeros(4, 12);
     for iFov = fov_list
         count = count + cellfun(@numel, mutation_table{iFov});
@@ -664,6 +525,7 @@ function final_rate = compile_mutation_rate(mutation_table, fov_list, poi_counts
         fprintf('%s : %d\n', bases(icyc), sum(sum(poi_counts(:, cyc))));
     end
     
+    % Sort results
     final_rate = final_rate([3,4,1,2], :);
     final_rate = final_rate(:, [3,4,1,2]);
     
@@ -672,6 +534,7 @@ function final_rate = compile_mutation_rate(mutation_table, fov_list, poi_counts
 end
 
 function [cycle_mutation_rate, by_channel_mutation_rates] = compile_cycle_mutation_rate(mutation_table, fov_list, poi_counts)
+    % Count total mutation counts
     by_channel_mutation_rates = zeros(4, 12, size(poi_counts, 1));
     for iFov = fov_list
         by_channel_mutation_rates(:, :, iFov) = cellfun(@numel, mutation_table{iFov}) ./ poi_counts(iFov, :);
@@ -681,6 +544,7 @@ function [cycle_mutation_rate, by_channel_mutation_rates] = compile_cycle_mutati
         count = count + cellfun(@numel, mutation_table{iFov});
     end
     
+    % Store results
     cycle_mutation_rate = zeros(4, 12);
     for icyc = 1:12
         cycle_mutation_rate(:, icyc) = sum(count(:, icyc), 2) / sum(poi_counts(:, icyc));
@@ -691,6 +555,7 @@ function compile_results_with_error_bars_8(counts, fovs, file_dir, poi_counts)
     n_groups = floor(length(fovs) / 8);
     count = cell(n_groups, 1);
     nPoi_list = cell(n_groups, 1);
+    % Split FOV's into groups of 8 and calculate mutation rates
     for igroup = 0:n_groups - 1
         ifov = fovs(igroup * 8 + 1 : (igroup + 1) * 8);
         icount = zeros(4, 12);
@@ -720,9 +585,11 @@ function compile_results_with_error_bars(mutation_rates, Fovs, file_dir)
     
     % A
     mut_to = [];
+    % Collect Mutation ratios
     for ig = Fovs
         mut_to = [mut_to, mutation_rates{ig}(:, cycles{3})];
     end
+    % Acquire standard deviation and mean
     sd_a = std(mut_to, [], 2) / sqrt(size(mut_to, 2));
     mn_a = mean(mut_to, 2);
     fig = plot_with_error_bars(mn_a([1,2,4]), sd_a([1,2,4]), {'A to G','A to T','A to C'});
